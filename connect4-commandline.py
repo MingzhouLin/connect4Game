ff64bd252676e3cff3e30046d8b5c0de26dc88b1
林铭洲 <linmingzhou@hayashibookpuro.wireless.concordia.ca>
2019-02-14 19:59:02 -0500

    origin version

--- connect4-commandline.py
+++ connect4-commandline.py
@@ -51,14 +51,14 @@ def is_valid_location(board, pos, type):
         for tuple in pos:
             if tuple[0] < 'A' or tuple[0] > 'H' or int(tuple[1]) > 12 or int(tuple[1]) < 1:
                 return False
-            if board[int(tuple[1])-1][ord(tuple[0]) - ord('A')] != 0:
+            if board[int(tuple[1]) - 1][ord(tuple[0]) - ord('A')] != 0:
                 return False
             if int(tuple[1]) - 1 >= 1 and board[int(tuple[1]) - 2][ord(tuple[0]) - ord('A')] == 0:
                 return False
     elif type == "2" or type == "4" or type == "6" or type == "8":
         if pos[0][0] < 'A' or pos[0][0] > 'H' or int(pos[0][1]) > 12 or int(pos[0][1]) < 1:
             return False
-        if board[int(pos[0][1])-1][ord(pos[0][0]) - ord('A')] != 0:
+        if board[int(pos[0][1]) - 1][ord(pos[0][0]) - ord('A')] != 0:
             return False
         if int(pos[0][1]) - 1 >= 1 and board[int(pos[0][1]) - 2][ord(pos[0][0]) - ord('A')] == 0:
             return False
@@ -82,7 +82,7 @@ def is_recycle_legal(origin_pos, new_pos, origin_pos_str, step_record, new_pos_s
     if int(origin_type) % 2 == 1:
         # 1,3,5,7
         coordinate = [get_upper_coordinate(origin_pos[0]), get_upper_coordinate(origin_pos[1])]
-        if dot_board[coordinate[0][0]][coordinate[0][1]] != 0 or dot_board[coordinate[1][0]][coordinate[1][1]]!=0:
+        if dot_board[coordinate[0][0]][coordinate[0][1]] != 0 or dot_board[coordinate[1][0]][coordinate[1][1]] != 0:
             print("you cannot recycle a card that has something on top of it.")
             return False
 
@@ -93,7 +93,6 @@ def is_recycle_legal(origin_pos, new_pos, origin_pos_str, step_record, new_pos_s
         if dot_board[coordinate[1][0]][coordinate[1][1]] != 0:
             print("you cannot recycle a card that has something on top of it.")
             return False
-
 
     if origin_pos_str == new_pos_str and origin_type == new_type:
         print("You cannot take one piece and put it back with no changes.")
@@ -159,6 +158,35 @@ def winning_move(board, piece):
     return False
 
 
+def is_game_over(dot_board, piece_pos, player1, player2):
+    dot_win = winning_move(dot_board, piece_pos)
+    color_win = winning_move(color_board, piece_pos)
+    if dot_win and color_win:
+        if turn == 0:
+            if player1 == "1":
+                print("Player 1 Wins with dot!")
+            else:
+                print("Player 1 Wins with color!")
+        else:
+            if player2 == "1":
+                print("Player 2 Wins with dot!")
+            else:
+                print("Player 2 Wins with color!")
+        return True
+    elif dot_win:
+        if player1 == "1":
+            print("Player 1 Wins with dot!")
+        else:
+            print("Player 2 Wins with dot!")
+        return True
+    elif color_win:
+        if player1 == "2":
+            print("Player 1 Wins with color!")
+        else:
+            print("Player 2 Wins with color!")
+        return True
+
+
 dot_board = create_board()
 color_board = create_board()
 print(dot_board)
@@ -168,54 +196,25 @@ turn = 0
 recycle = False
 step_counter = 1
 step_record = dict()
+
 player1 = input("Player 1 choose side: 1.dot; 2.color")
 player2 = input("Player 2 choose side: 1.dot; 2.color")
-
-def read_multiple_lines():
-    input_list = list()
-    while True:
-        line = stdin.readline().strip()
-        if  len(line) < 3:
-            return input_list
-        input_list.append(line)
-
-
-
-input_type = input("Please choose the input type. (1. step-by-step 2. multiple steps)")
-input_list = list()
-if input_type == "2":
-    print("Please input the multiple steps")
-    input_list = read_multiple_lines()
-
-
-
 
 while not game_over:
     if step_counter > 60:
         print("No player has won, game ends.")
         break
     # Ask for Player 1 Input
-    if input_type == "2" and step_counter > len(input_list):
-        print("The multiple steps have been handled.")
-        print("Please input a step by hand.")
-        input_type = "1"
-
-    if not recycle and input_type == "1":
+    if not recycle:
         if turn == 0:
             string = input("Player 1 turn: ")
         else:
             string = input("Player 2 turn: ")
-    elif input_type == "1":
+    else:
         if turn == 0:
             string = input("Player 1 turn(recycle): ")
         else:
             string = input("Player 2 turn(recycle): ")
-    elif input_type == "2" and step_counter <= len(input_list):
-        string = input_list[step_counter - 1]
-
-
-
-
     string = string.split(" ")
 
     if len(string) == 4:
@@ -232,9 +231,6 @@ while not game_over:
         else:
             print("The operation is illegal")
             print(string)
-            if input_type == "2":
-                new_step = input("Please input a valid move.")
-                input_list[step_counter - 1] = new_step
             continue
     else:
         if recycle is False:
@@ -249,9 +245,6 @@ while not game_over:
         new_pos_str = to_string(new_pos)
 
         if not is_recycle_legal(origin_pos, new_pos, origin_pos_str, step_record, new_pos_str, new_type, step_counter):
-            if input_type == "2":
-                new_step = input("Please input a valid recycle.")
-                input_list[step_counter - 1] = new_step
             continue
 
         fake_dot_board = dot_board
@@ -264,37 +257,8 @@ while not game_over:
             drop_piece(dot_board, color_board, new_pos, new_type, step_record, step_counter)
         else:
             print("Please select a valid place on the board to recycle.")
-            if input_type == "2":
-                new_step = input("Please input a valid recycle.")
-                input_list[step_counter - 1] = new_step
             continue
-
-    dot_win = winning_move(dot_board, piece_pos)
-    color_win = winning_move(color_board, piece_pos)
-    if dot_win and color_win:
-        if turn == 0:
-            if player1 == "1":
-                print("Player 1 Wins with dot!")
-            else:
-                print("Player 1 Wins with color!")
-        else:
-            if player2 == "1":
-                print("Player 2 Wins with dot!")
-            else:
-                print("Player 2 Wins with color!")
-        game_over = True
-    elif dot_win:
-        if player1 == "1":
-            print("Player 1 Wins with dot!")
-        else:
-            print("Player 2 Wins with dot!")
-        game_over = True
-    elif color_win:
-        if player1 == "2":
-            print("Player 1 Wins with color!")
-        else:
-            print("Player 2 Wins with color!")
-        game_over = True
+    game_over = is_game_over(dot_board, piece_pos, player1, player2)
     print("Dot board    " + str(step_counter) + " round.   dot->1:black,2:white")
     print_board(dot_board)
     print("Color board    " + str(step_counter) + " round.   color->1:red, 2:white")
